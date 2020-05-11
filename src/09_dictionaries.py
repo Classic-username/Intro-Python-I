0e5b761dfdfffabc83c87bdca2b2378122a3f9d7
Joshua Draper <josh.drapergame@gmail.com>
2020-05-10 22:51:06 -0600

    Finished assignment

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -41,7 +41,14 @@ waypoints = [
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
+
 # YOUR CODE HERE
 
+waypoints[0]['lon'] = -130
+waypoints[0]['name'] = "not a real place"
+
+print(waypoints[0])
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for val in waypoints:    print(val.values())
--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,23 @@
 
 # YOUR CODE HERE
 
+def is_even(num):
+    print(num % 2 == 0)
+
+
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+
+is_even(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+if num % 2 == 0:
+    print('Even!')
+else:
+    print('Odd!')
